86c2cd30194c877d2ca1522a0bf40c2e1afab75e
Володимир Чередниченко <vladnick@ukr.net>
2022-08-08 19:08:15 +0300

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,9 @@
 import { getData } from "./getData.js";
 import { renderGallery } from "./renderGallery.js";
 
-const init = async () => {
+const init = async (selectorWrapper) => {
+  const wrapper=document.querySelector(selectorWrapper)
   const photos = await getData();
-  renderGallery(photos);
+  renderGallery(wrapper,photos);
 };
-init();
+init(".gallery__wrapper");
