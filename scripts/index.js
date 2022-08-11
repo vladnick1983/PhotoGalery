8597ee1fbe840f3c4e2fb316757199094eb21d3e
Володимир Чередниченко <vladnick@ukr.net>
2022-08-11 19:33:45 +0300

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,25 @@
 import { getData } from "./getData.js";
 import { renderGallery } from "./renderGallery.js";
+import { renderPhoto } from "./renderPhoto.js";
 
-const init = async (selectorWrapper) => {
-  const wrapper=document.querySelector(selectorWrapper)
-  const photos = await getData();
-  renderGallery(wrapper,photos);
+const init = async ({ selectorGalleryWrapper, selectorPhotoWrapper }) => {
+  const galleryWrapper = document.querySelector(selectorGalleryWrapper);
+  const photoWrapper = document.querySelector(selectorPhotoWrapper);
+
+  if (galleryWrapper) {
+    const photos = await getData("data.json");
+    renderGallery(galleryWrapper, photos);
+  }
+  if (photoWrapper) {
+    const url = new URL(location.href);
+    console.log(url.searchParams.get("photo"));
+
+    const photo = await getData("photo.json");
+    renderPhoto(photoWrapper, photo);
+  }
 };
-init(".gallery__wrapper");
+
+init({
+  selectorGalleryWrapper: ".gallery__wrapper",
+  selectorPhotoWrapper: ".photo__wrapper",
+});
