3e8321269f632273d557b2e51598e924fea660ea
Володимир Чередниченко <vladnick@ukr.net>
2022-08-12 11:39:15 +0300

    Update getData.js

--- scripts/getData.js
+++ scripts/getData.js
@@ -1,4 +1,18 @@
-export const getData = (url) => {
+import { ACCESS_KEY, API_URL_PHOTOS } from "./const.js";
+
+export const getData = ({ page = 1, count, idPhoto }) => {
+  const url = new URL(API_URL_PHOTOS);
+
+  url.searchParams.set("client_id", ACCESS_KEY);
+
+  if (count && page) {
+    url.searchParams.append("per_page", count);
+    url.searchParams.append("page", page);
+  }
+
+  if (idPhoto) {
+    url.pathname += `/${idPhoto}`;
+  }
   return fetch(url).then((data) => {
     return data.json();
   });
