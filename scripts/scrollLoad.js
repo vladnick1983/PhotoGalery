06b27a01f622fac5b4cbca4c00280630cebaff65
Володимир Чередниченко <vladnick@ukr.net>
2022-08-12 11:39:25 +0300

    Update scrollLoad.js

--- scripts/scrollLoad.js
+++ scripts/scrollLoad.js
@@ -2,12 +2,13 @@ import { createCardPhoto } from "./createCardPhoto.js";
 import { getData } from "./getData.js";
 
 export const scrollLoad = (gallery, grid, endElement) => {
+  let i = 1;
   const observer = new IntersectionObserver(
     async (entries) => {
       if (entries[0].isIntersecting) {
-        const photos = await getData("data.json");
+        const photos = await getData({ page: i++, count: 30 });
         const cards = photos.map(createCardPhoto);
-        
+
         Promise.all(cards).then((cards) => {
           gallery.append(...cards);
           grid.appended(cards);
