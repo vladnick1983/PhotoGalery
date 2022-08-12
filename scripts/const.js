00c21631f9f6467dab3f80ab35c97cc5a03ef008
Володимир Чередниченко <vladnick@ukr.net>
2022-08-12 11:39:11 +0300

    Create const.js

---
+++ scripts/const.js
@@ -0,0 +1,4 @@
+export const API_URL = "https://api.unsplash.com";
+export const API_URL_PHOTOS = `${API_URL}/photos`;
+export const ACCESS_KEY = "MEdJvgVusFl0tq1BgOgjzLwCf_9_RkRC_pNFFyPZSDc";
+export const SECRET_KEY = "F3SVMv0cG7Ld1BAf-J38P-BxWrn3rF7EspgVL14JKjw";
